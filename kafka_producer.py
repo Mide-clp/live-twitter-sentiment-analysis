8f0c0cd310a4afa707f08f658e8bd62da627d4e3
Mide clp <aogunnola@gmail.com>
2022-03-19 18:57:25 +0100

    create kafka producer

---
+++ kafka_producer.py
@@ -0,0 +1,3 @@
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
--- twitter-producer.py
+++ twitter-producer.py
@@ -1,8 +1,8 @@
 import json
 import os
 from dotenv import load_dotenv
-from tweepy import Stream, StreamingClient
-
+from tweepy import Stream
+from kafka_producer import producer
 # load env keys
 load_dotenv()
 
@@ -10,12 +10,19 @@ ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 API_KEY = os.environ.get("API_KEY")
 API_SECRET = os.environ["API_SECRET"]
+TOPIC = "tweets_loader"
+TWEET_KEYWORDS = ["covid19", "corona virus"]
 
 
 # creating stream listener
 class MYStreamListener(Stream):
     def on_data(self, raw_data):
         data = json.loads(raw_data)
+
+        # convert data to bytes and load into kafka producer
+        tweets = bytearray(data.encode("utf-8"))
+        producer.send(TOPIC, tweets)
+
         print(data)
         file = json.dumps(data)
         with open("data.json", "a") as f:
@@ -36,4 +43,4 @@ def stream_tweets(word):
 
 
 # start streaming twitter data
-stream_tweets(["covid19", "corona virus"])
+stream_tweets(TWEET_KEYWORDS)
