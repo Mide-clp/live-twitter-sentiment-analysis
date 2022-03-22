a2da67ea974d3ba8b3aedbda36f2f46394f24481
Mide clp <aogunnola@gmail.com>
2022-03-22 11:51:16 +0100

    create sentiment score on data

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # live-twitter-sentiment-analysis
 
-
-'''
-
-'''
+## Run spark job
+```
+spark-submit --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1 spark_streaming.py
+```
--- spark_streaming.py
+++ spark_streaming.py
@@ -1,14 +1,81 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
-# from pyspark.
+from pyspark.sql.types import FloatType
+from textblob import TextBlob
+
+# from pyspark.sql.functions import r
 TOPIC = "tweets_loader"
 
+
+# clean tweet texts by removing hashtags, line, @ and RT and...
+def clean_tweet(lines):
+    text_tweet = df.select(func.col("value").cast("string"))
+    words = text_tweet.select(func.explode(func.split(func.col("value"), "t_text")).alias("word"))
+    words = words.na.replace("", None)
+    words = words.na.drop()
+    words = words.withColumn("word", func.regexp_replace("word", r"http\S+", ""))
+    words = words.withColumn("word", func.regexp_replace("word", "@\w+", ""))
+    words = words.withColumn("word", func.regexp_replace("word", "#", ""))
+    words = words.withColumn("word", func.regexp_replace("word", "RT", ""))
+    words = words.withColumn("word", func.regexp_replace("word", ":", ""))
+    return words
+
+
+# apply sentiment analysis
+
+# get polarity score
+# Polarity lies between [-1,1], -1 defines a negative sentiment and 1 defines a positive sentiment.
+def getpolarity_score(text):
+    return TextBlob(text).sentiment.polarity
+
+
+# get subjectivity score
+# Subjectivity lies between [0,1] Subjectivity quantifies the amount of personal opinion and factual information
+# contained in the text. The higher subjectivity means that the text contains personal opinion rather than factual information.
+def getsubjectivity_score(text):
+    return TextBlob(text).sentiment.subjectivity
+
+
+def text_sentiment(word):
+    # creating user defined function (udf) for use in spark dataframe
+    getpolarity_score_udf = func.udf(getpolarity_score, FloatType())
+    getsubjectivity_score_udf = func.udf(getsubjectivity_score, FloatType())
+
+    word_sentiment_score = word.withColumn("polarity_score", getpolarity_score_udf(func.col("word")))
+    word_sentiment_subjectivity = word_sentiment_score.withColumn("subjectivity_score",
+                                                                  getsubjectivity_score_udf(func.col("word")))
+
+    return word_sentiment_subjectivity
+
+
+# create user defined function (udf)
+
+
 # create spark session
-spark = SparkSession.builder.master("local[*]").appName("Stream-twitter-data").getOrCreate()
+if __name__ == "__main__":
+    spark = SparkSession.builder.master("local[*]").appName("Stream-twitter-data").getOrCreate()
 
-df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", TOPIC) \
-    .option("startingOffsets", "latest").load()
+    # read tweet from kafka
+    df = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", TOPIC) \
+        .option("startingOffsets", "latest").load()
 
-df.printSchema()
+    # clean the data
+    words_df = clean_tweet(df)
 
-# spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.13:3.2.1 spark_streaming.py
+    # analyze text to define polarity and subjectivity
+    word_sentiment = text_sentiment(words_df)
+
+    query = word_sentiment.writeStream \
+        .format("console") \
+        .outputMode("append") \
+        .queryName("word") \
+        .start()
+
+    query.awaitTermination()
+
+    spark.stop()
+
+# spark-submit --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1 spark_streaming.py
